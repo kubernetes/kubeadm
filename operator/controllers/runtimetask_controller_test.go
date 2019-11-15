360fd2fedd378e2c130f65762358bbbd4d7668d7
fabriziopandini <fpandini@vmware.com>
2019-11-15 10:10:47 +0100

    fix-verify checks

--- operator/controllers/runtimetask_controller_test.go
+++ operator/controllers/runtimetask_controller_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controllers
 
 import (
@@ -6,8 +22,9 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/record"
-	"k8s.io/kubeadm/operator/errors"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/log"
+
+	"k8s.io/kubeadm/operator/errors"
 
 	operatorv1 "k8s.io/kubeadm/operator/api/v1alpha1"
 )
