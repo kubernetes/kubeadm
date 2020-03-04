ce597555b50aee23c7245f0172f7f98ba1e32599
Lubomir I. Ivanov <lubomirivanov@vmware.com>
2020-03-04 17:08:58 +0200

    tests/e2e/manifests: pin the go version and install it using curl

--- tests/e2e/manifests/verify_manifest_lists.sh
+++ tests/e2e/manifests/verify_manifest_lists.sh
@@ -16,21 +16,29 @@ trap "rm go.mod go.sum" EXIT
 
 # install curl if missing
 if ! `curl --version > /dev/null`; then
-	apt-get update || exit 1
-	apt-get install -y curl || exit 1
+	apt-get update
+	apt-get install -y curl
 fi
 
 # install go if missing
 if ! `go version > /dev/null`; then
-	apt-get update || exit 1
-	apt-get install -y golang-go || exit 1
+	curl https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz -o /tmp/go.tar.gz
+	tar -C /usr/local -xzf /tmp/go.tar.gz
+	export PATH="$PATH":/usr/local/go/bin
+fi
+
+# api-machinery requires gcc
+#   go: extracting k8s.io/apimachinery v0.17.3
+#   runtime/cgo
+#   exec: "gcc": executable file not found in $PATH
+if ! `gcc -v > /dev/null`; then
+	apt-get install -y gcc
 fi
 
 LPATH=`dirname "$0"`
 cd "$LPATH"
 
 # use go modules. this forces using the latest k8s.io/apimachinery package.
-export GO111MODULE=on
 go mod init verify-manifest-lists
 
 # run unit tests
