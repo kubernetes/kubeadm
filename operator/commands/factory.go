6dab0a228a306a22f385e5e0a7a2ce58ce851eb2
Zouyu <zouy.fnst@cn.fujitsu.com>
2020-03-12 10:06:29 +0800

    add null point check for operator/commands/factory.go
    Signed-off-by: Zouyu <zouy.fnst@cn.fujitsu.com>

--- operator/commands/factory.go
+++ operator/commands/factory.go
@@ -27,51 +27,52 @@ import (
 
 // RunCommand run the command on a node
 func RunCommand(c *operatorv1.CommandDescriptor, log logr.Logger) error {
+	if c != nil {
+		if c.Preflight != nil {
+			return runPreflight(c.Preflight, log)
+		}
 
-	if c.Preflight != nil {
-		return runPreflight(c.Preflight, log)
-	}
+		if c.KubeadmRenewCertificates != nil {
+			return runKubeadmRenewCertificates(c.KubeadmRenewCertificates, log)
+		}
 
-	if c.KubeadmRenewCertificates != nil {
-		return runKubeadmRenewCertificates(c.KubeadmRenewCertificates, log)
-	}
+		if c.KubeadmUpgradeApply != nil {
+			return runKubeadmUpgradeApply(c.KubeadmUpgradeApply, log)
+		}
 
-	if c.KubeadmUpgradeApply != nil {
-		return runKubeadmUpgradeApply(c.KubeadmUpgradeApply, log)
-	}
+		if c.KubeadmUpgradeNode != nil {
+			return runKubeadmUpgradeNode(c.KubeadmUpgradeNode, log)
+		}
 
-	if c.KubeadmUpgradeNode != nil {
-		return runKubeadmUpgradeNode(c.KubeadmUpgradeNode, log)
-	}
+		if c.KubectlDrain != nil {
+			return runKubectlDrain(c.KubectlDrain, log)
+		}
 
-	if c.KubectlDrain != nil {
-		return runKubectlDrain(c.KubectlDrain, log)
-	}
+		if c.KubectlUncordon != nil {
+			return runKubectlUncordon(c.KubectlUncordon, log)
+		}
 
-	if c.KubectlUncordon != nil {
-		return runKubectlUncordon(c.KubectlUncordon, log)
-	}
+		if c.UpgradeKubeadm != nil {
+			return runUpgradeKubeadm(c.UpgradeKubeadm, log)
+		}
 
-	if c.UpgradeKubeadm != nil {
-		return runUpgradeKubeadm(c.UpgradeKubeadm, log)
-	}
+		if c.UpgradeKubeletAndKubeactl != nil {
+			return runUpgradeKubectlAndKubelet(c.UpgradeKubeletAndKubeactl, log)
+		}
 
-	if c.UpgradeKubeletAndKubeactl != nil {
-		return runUpgradeKubectlAndKubelet(c.UpgradeKubeletAndKubeactl, log)
-	}
+		if c.Pass != nil {
+			return nil
+		}
 
-	if c.Pass != nil {
-		return nil
-	}
+		if c.Fail != nil {
+			time.Sleep(5 * time.Second)
+			return errors.New("command fail failed")
+		}
 
-	if c.Fail != nil {
-		time.Sleep(5 * time.Second)
-		return errors.New("command fail failed")
-	}
-
-	if c.Wait != nil {
-		time.Sleep(time.Duration(c.Wait.Seconds) * time.Second)
-		return nil
+		if c.Wait != nil {
+			time.Sleep(time.Duration(c.Wait.Seconds) * time.Second)
+			return nil
+		}
 	}
 
 	return errors.New("invalid Task.Spec.[]CommandDescriptor. There are no command implementations matching this spec")
